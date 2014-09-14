e93e44fc562457b96cd94f32e6b06a708abe2eed
Ethan Resnick <ethan.resnick@gmail.com>
2014-09-13 23:04:25 -0400

    Bower config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,20 @@
 grunt.loadNpmTasks('grunt-bower-concat');
+
+grunt.initConfig({
+  bower_concat: {
+    all: {
+    dest: 'build/_bower.js',
+    exclude: [
+      'jquery',
+      'modernizr'
+    ],
+    dependencies: {
+      'underscore': 'jquery',
+      'backbone': 'underscore',
+    },
+    bowerOptions: {
+      relative: false
+    }
+    }
+  }
+  });
