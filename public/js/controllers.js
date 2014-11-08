fa84255b603602cdad81e4a6ef95d895d2ca3425
Max Dumas <maltor124@gmail.com>
2014-11-07 22:32:29 -0500

    Created skeleton for the rest of the fields on the form, and added basic submission logic.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,13 +2,25 @@
 
 angular.module('app.controllers', []).
   controller('EventAddCtrl', function ($scope, $http) {
-    $scope.event = {};
+    $scope.event = { addedBy: "Max" };
 
     $http.get("https://api.tnyu.org/teams")
-    .success(function(data){
-    	console.log(data);
-    	$scope.teams = data.teams;
-    }).error(function(data, status){
-    	console.log(status);
-    });
+        .success(function(data){
+        	console.log(data);
+        	$scope.teams = data.teams;
+        })
+        .error(function(data, status){
+        	console.log(status);
+        });
+
+    $scope.submit = function() {
+        console.log($scope.event);
+        $http.post('http://posttestserver.com/post.php', $scope.event)
+            .success(function(data) {
+                console.log(data);
+            })
+            .error(function(data, status) {
+                console.log(status);
+            });
+    }
   });
