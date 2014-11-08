c4c820fde6ed9e00c1bba7227f55b88c0ef8b6fc
Max Dumas <maltor124@gmail.com>
2014-11-07 23:08:22 -0500

    Added team selection logic

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,8 +3,9 @@
 angular.module('app.controllers', []).
   controller('EventAddCtrl', function ($scope, $http) {
     $scope.event = { addedBy: "Max" };
+    $scope.selectedTeams = {};
 
-    $http.get("https://api.tnyu.org/teams")
+    $http.get("https://api.tnyu.org/v1.0/teams")
         .success(function(data){
         	console.log(data);
         	$scope.teams = data.teams;
@@ -13,8 +14,21 @@ angular.module('app.controllers', []).
         	console.log(status);
         });
 
+    $scope.toggleTeam = function(teamid) {
+        console.log(teamid);
+        if($scope.selectedTeams[teamid]) 
+            delete $scope.selectedTeams[teamid];
+        else $scope.selectedTeams[teamid] = true;
+    }
+
     $scope.submit = function() {
+
+        // Aggregrate all selected teams into our event to be submitted.
+        $scope.event.teams = [];
+        for(var teamid in Object.keys($scope.selectedTeams))
+            $scope.event.teams.push(teamid);
         console.log($scope.event);
+
         $http.post('http://posttestserver.com/post.php', $scope.event)
             .success(function(data) {
                 console.log(data);
