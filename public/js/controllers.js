7f05e9c7dee6f5ae2ed56518be920ee7bd855417
Cheryl Wu <grungerabbit@gmail.com>
2014-11-07 20:59:35 -0500

    add teams to controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,4 +3,12 @@
 angular.module('app.controllers', []).
   controller('EventAddCtrl', function ($scope, $http) {
     $scope.event = {};
+
+    $http.get("https://api.tnyu.org/teams")
+    .success(function(data){
+    	console.log(data);
+    	$scope.teams = data.teams;
+    }).error(function(data, status){
+    	console.log(status);
+    });
   });
