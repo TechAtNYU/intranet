3c851869f524aa97e708299dd40928e59e88b4b9
Max Dumas <maltor124@gmail.com>
2014-11-08 22:45:15 -0500

    Incorporated modal work from https://github.com/justindeguzman/IntranetModal

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,12 @@
 'use strict';
 
 angular.module('app', [
+  'ngRoute',
+  'ui.bootstrap',
   'app.controllers',
   'app.filters',
   'app.services',
   'app.directives',
-  'ngRoute'
 ]).config(function ($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true);
 });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,53 +1,88 @@
 'use strict';
 
-angular.module('app.controllers', []).
-  controller('EventAddCtrl', function ($scope, $http, userData) {
-    // User Initialization
-    (function() {
-        userData.getInformation(function(data){
-            $scope.currentUser = data;
-            if (userData.isLoggedIn($scope.currentUser)) {
-                console.log("User logged in");
-            }
-            userData.onTeams($scope.currentUser, function(teamData){
-                console.log(teamData);
-            });
-        });
-    })();
+var controllers = angular.module('app.controllers', [])
 
-    // Data Initialization
-    $scope.event = { addedBy: "Max" };
-    $scope.selectedTeams = {};
-    $http.get("https://api.tnyu.org/v1.0/teams")
-        .success(function(data){
-        	console.log(data);
-        	$scope.teams = data.teams;
-        })
-        .error(function(data, status){
-        	console.log(status);
-        });
+controllers.controller('EventAddCtrl', function ($scope, $http, $modal, userData) {
+  // User Initialization
+  (function() {
+    userData.getInformation(function(data){
+      $scope.currentUser = data;
+      if (userData.isLoggedIn($scope.currentUser)) {
+        console.log("User logged in");
+      }
+      userData.onTeams($scope.currentUser, function(teamData){
+        console.log(teamData);
+      });
+    });
+  })();
 
-    $scope.toggleTeam = function(teamid) {
-        console.log(teamid);
-        if($scope.selectedTeams[teamid])
-            delete $scope.selectedTeams[teamid];
-        else $scope.selectedTeams[teamid] = true;
-    }
+  // Data Initialization
+  $scope.event = { addedBy: "Max", links: {} };
+  $scope.selectedTeams = {};
+  $http.get("https://api.tnyu.org/v1.0/teams")
+    .success(function(data){
+      console.log(data);
+      $scope.teams = data.teams;
+    })
+    .error(function(data, status){
+      console.log(status);
+    });
 
-    $scope.submit = function() {
-        // Aggregrate all selected teams into our event to be submitted.
-        $scope.event.teams = [];
-        for(var teamid in Object.keys($scope.selectedTeams))
-            $scope.event.teams.push(teamid);
+  $scope.toggleTeam = function(teamid) {
+    console.log(teamid);
+    if($scope.selectedTeams[teamid])
+      delete $scope.selectedTeams[teamid];
+    else $scope.selectedTeams[teamid] = true;
+  }
 
-        $http.post('https://api.tnyu.org/v1.0/events', 
-                    $scope.event, 
-                    { headers: { "Content-Type": "application/vnd.api+json" } })
-            .success(function(data) {
-                console.log(data);
-            })
-            .error(function(data, status) {
-                console.log(status);
-            });
-    }
+  $scope.submit = function() {
+    // Aggregrate all selected teams into our event to be submitted.
+    $scope.event.teams = [];
+    for(var teamid in Object.keys($scope.selectedTeams))
+      $scope.event.links.teams.push(teamid);
+
+    $http.post('https://api.tnyu.org/v1.0/events', 
+          $scope.event, 
+          { headers: { "Content-Type": "application/vnd.api+json" } })
+      .success(function(data) {
+        console.log(data);
+      })
+      .error(function(data, status) {
+        console.log(status);
+      });
+  }
+
+  $scope.addPresenter = function addPresenter() {
+    $modal.open({
+      templateUrl: '/partials/presenter.html',
+      controller: 'AddPresenterCtrl'
+    });
+  };
+
+  $scope.addVenue = function addVenue() {
+  $modal.open({
+    templateUrl: '/partials/venue.html',
+    controller: 'AddVenueCtrl'
   });
+  };
+});
+
+controllers.controller('AddPresenterCtrl', function($scope, $modalInstance) {
+  $scope.ok = function() {
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
+});
+
+controllers.controller('AddVenueCtrl', function($scope, $modalInstance) {
+  $scope.ok = function() {
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
+});
