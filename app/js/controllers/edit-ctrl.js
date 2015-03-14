f927957970f6b018fdf884028db6f9829917df46
Clayton Albachten <clay.reed.a@gmail.com>
2015-03-14 19:11:27 -0400

    Connect edit inputs to Angular models

--- app/js/controllers/edit-ctrl.js
+++ app/js/controllers/edit-ctrl.js
@@ -6,10 +6,25 @@ angular
 
   var resourceName = $stateParams.resourceName,
       resourceId = //$stateParams.id; 
-      '5403bec2627d5f43512ce1dc';
+      '53f54dd98d1e62ff12539dbb';
 
-  $scope.presenter = Restangular
+  
+
+  Restangular
     .one(resourceName, resourceId)
-    .get().$object;
+    .get()
+      .then(function(presenter){
+
+        // transform presenter from Array to String 
+        presenter.schools = presenter.schools.join(' ');
+
+        // yyyy-MM-dd INSANITY 
+        var d = new Date(presenter.graduationDate);
+        var yyyy = d.getFullYear().toString(),
+              mm = d.getMonth() + 1,
+              dd = d.getDate();
+        presenter.graduationDate = yyyy + '-' + (mm > 9 ? mm.toString() : '0' + mm) + '-' + (dd.toString() > 9 ? dd : '0' + dd);
+        $scope.presenter = presenter; 
+      })
 
 });
