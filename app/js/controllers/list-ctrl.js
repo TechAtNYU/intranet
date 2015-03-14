3083d5e07e751b16ac41ff98bae055ca5b2acdb4
Max Dumas <maltor124@gmail.com>
2015-03-14 16:17:56 -0400

    Fix list control

--- app/js/controllers/list-ctrl.js
+++ app/js/controllers/list-ctrl.js
@@ -3,10 +3,12 @@
 angular
 .module('app.controllers')
 .controller('ListCtrl', function($scope, $rootScope, $stateParams, Restangular) {
-	$scope.name = 'Presenters';
-
 	var resourceName = $stateParams.resourceName;
-	
+	$scope.resourceName = resourceName;
+	var selectionMode = $stateParams.selectionMode;
+	if(!selectionMode || (selectionMode !== 'single ' && selectionMode !== 'multiple')) {
+		selectionMode = 'multiple';
+	}
 	$scope.selectionMode = selectionMode;
 	$scope.models = Restangular.all(resourceName).getList().$object;
 });
