8945acd7b5455d087a7cae2541e091e3e96645f8
Max Dumas <maltor124@gmail.com>
2015-03-28 18:29:20 -0400

    Nested form elements within field views. Responses are now automatically flattened to handle nested object indexing ie with Person.contact

--- app/js/app.js
+++ app/js/app.js
@@ -14,16 +14,53 @@ angular.module('app', [
   'app.services',
   'app.directives',
 ]).config(function(RestangularProvider) {
-	RestangularProvider.setBaseUrl('https://api.tnyu.org/v1.0');
+	RestangularProvider.setBaseUrl('https://api.tnyu.org/v2');
 
 	// Configuring Restangular to work with JSONAPI spec
 	RestangularProvider.setDefaultHeaders({
-		'Content-Type': 'application/vnd.api+json; charset=utf-8'
+    'Accept': 'application/vnd.api+json, application/*, */*',
+		'Content-Type': 'application/vnd.api+json; ext=bulk'
 	});
 
 	RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
-		return data[Object.keys(data)[0]]; // Return the first child of the returned data
+    return data.data;
 	});
+
+  RestangularProvider.addResponseInterceptor(function(data, operation, what, url, response, deferred) {
+    console.log(data);
+
+    var flattenTree = function(resource) {
+      console.log(resource);
+      var flatten = function(object, parentKey) {
+        console.log(object);
+        if(_.isObject(object)) {
+          Object.keys(object).forEach(function(key) {
+            flatten(object[key], parentKey + '.' + key);
+          });
+        } else {
+          console.log(parentKey);
+          resource[parentKey] = object;
+        }
+      };
+
+      Object.keys(resource).forEach(function(key) {
+        console.log(key);
+        if(key === 'links') {
+          return;
+        } else if(!_.isArray(resource[key])) {
+          flatten(resource[key], key);
+        }
+      });
+    };
+
+    if(_.isArray(data)) {
+      data.forEach(flattenTree);
+    } else {
+      flattenTree(data);
+    }
+    console.log(data);
+    return data;
+  });
 }).config(function (datepickerConfig) {
   datepickerConfig.showWeeks = false;
 }).config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
@@ -64,11 +101,11 @@ angular.module('app', [
       controller: 'EditCtrl',
       controllerAs: 'ctrl',
       resolve: {
-        apiDescription: function($http) {
-          return $http.get('/data/resource-description.json')
+        apiDescription: function(Restangular) {
+          return Restangular.all('').getList()
           .then(function(data) {
             data = {
-              data: data.data.data,
+              data: data,
               resource: function(name) {
                 return _.find(data.data, function(r) {
                   return r.id === name;
--- app/js/controllers/edit-ctrl.js
+++ app/js/controllers/edit-ctrl.js
@@ -4,33 +4,17 @@ angular
 .module('app.controllers')
 .controller('EditCtrl', function($scope, $rootScope, $stateParams, $sce, $interval, Restangular, apiDescription, formElementProvider) {
   var resourceName = $stateParams.resourceName,
-      resourceId = //$stateParams.id; 
-      '53f54dd98d1e62ff12539dbb'; // test id 
+      resourceId = $stateParams.id;
+
+  var resource = Restangular.one(resourceName, resourceId);
   $scope.rdesc = apiDescription.resource(resourceName);
   $scope.fep = formElementProvider;
-  $scope.model = Restangular.one(resourceName, resourceId).get().$object;
+  $scope.model = resource.get().$object;
   $interval(function() { console.log($scope.model); }, 500);
 
-  // var resource = Restangular.one(resourceName, resourceId);
-
-  // resource.get()
-  //   .then(function(live) {
-  //     // // transform schools to String 
-  //     // presenter.schools = presenter.schools.join(',');
-
-  //     // // format date for input[type=date]
-  //     // if (presenter.graduationDate) {
-  //     //   presenter.graduationDate = presenter.graduationDate.formatForInputTypeDate(); // see js/lib/extentions.js  
-  //     // }
-    
-  //     // $scope.presenter = Restangular.stripRestangular(presenter);
-
-  //     $scope.model = 
-  //   });
-
   // FAKE, but more or less like this...  
-  // $scope.updateResource = function() {
-  //   console.log($scope.presenter);
-  //   // resource.put($scope.presenter); ClayReedA gets 403 Forbidden 
-  // };
+  $scope.updateResource = function() {
+    console.log($scope.model);
+    resource.put($scope.model);
+  };
 });
