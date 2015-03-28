595862bbfab5577ce626e0458d7d27f0c7498c37
Max Dumas <maltor124@gmail.com>
2015-03-28 15:16:07 -0400

    Edit form now pulls live data

--- app/js/controllers/edit-ctrl.js
+++ app/js/controllers/edit-ctrl.js
@@ -8,27 +8,29 @@ angular
       '53f54dd98d1e62ff12539dbb'; // test id 
   $scope.rdesc = apiDescription.resource(resourceName);
   $scope.fep = formElementProvider;
-  $scope.model = {};
-  console.log($scope.rdesc);
+  $scope.model = Restangular.one(resourceName, resourceId).get().$object;
   $interval(function() { console.log($scope.model); }, 500);
-  var resource = Restangular.one(resourceName, resourceId);
 
-  resource.get()
-    .then(function(presenter) {
-      // // transform schools to String 
-      // presenter.schools = presenter.schools.join(',');
+  // var resource = Restangular.one(resourceName, resourceId);
 
-      // // format date for input[type=date]
-      // if (presenter.graduationDate) {
-      //   presenter.graduationDate = presenter.graduationDate.formatForInputTypeDate(); // see js/lib/extentions.js  
-      // }
+  // resource.get()
+  //   .then(function(live) {
+  //     // // transform schools to String 
+  //     // presenter.schools = presenter.schools.join(',');
+
+  //     // // format date for input[type=date]
+  //     // if (presenter.graduationDate) {
+  //     //   presenter.graduationDate = presenter.graduationDate.formatForInputTypeDate(); // see js/lib/extentions.js  
+  //     // }
     
-      // $scope.presenter = Restangular.stripRestangular(presenter);
-    });
+  //     // $scope.presenter = Restangular.stripRestangular(presenter);
+
+  //     $scope.model = 
+  //   });
 
   // FAKE, but more or less like this...  
-  $scope.updateResource = function() {
-    console.log($scope.presenter);
-    // resource.put($scope.presenter); ClayReedA gets 403 Forbidden 
-  };
+  // $scope.updateResource = function() {
+  //   console.log($scope.presenter);
+  //   // resource.put($scope.presenter); ClayReedA gets 403 Forbidden 
+  // };
 });
