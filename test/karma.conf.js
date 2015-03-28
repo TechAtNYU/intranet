2d986315b3adc544211057dbff200c808bfd99bd
Max Dumas <maltor124@gmail.com>
2015-03-28 15:21:34 -0400

    Update karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -16,24 +16,24 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
     // App Dependencies
-      "http://code.jquery.com/jquery-2.1.1.min.js",
-      "http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular.js",
-      "http://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.3.1/lodash.min.js",
-      "http://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.13/angular-ui-router.min.js",
-      "http://cdnjs.cloudflare.com/ajax/libs/restangular/1.3.1/restangular.js",
-      "http://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.11.2/ui-bootstrap-tpls.min.js",
-      "./app/js/lib/datetimepicker-tpls-0.11.js",
-      "./app/js/lib/multi-select.js",
+      'http://code.jquery.com/jquery-2.1.1.min.js',
+      'http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular.js',
+      'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.3.1/lodash.min.js',
+      'http://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.13/angular-ui-router.min.js',
+      'http://cdnjs.cloudflare.com/ajax/libs/restangular/1.3.1/restangular.js',
+      'http://cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.11.2/ui-bootstrap-tpls.min.js',
+      './app/js/lib/datetimepicker-tpls-0.11.js',
+      './app/js/lib/multi-select.js',
 
     // App Files
-      "./app/js/app.js",
-      "./app/js/directives.js",
-      "./app/js/filters.js",
-      "./app/js/services.js",
-      "./app/js/controllers/*.js",
+      './app/js/app.js',
+      './app/js/directives.js',
+      './app/js/filters.js',
+      './app/js/services.js',
+      './app/js/controllers/*.js',
 
     // Unit Test Dependencies
-      "http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-mocks.js",
+      'http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-mocks.js',
 
     // Unit Tests
       './test/unit/**/*.js'
