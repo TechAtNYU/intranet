2a92313dc78118004cf4bed2447149813ef6046a
Clayton Albachten <clay.reed.a@gmail.com>
2015-04-01 21:43:28 -0400

    Scaffold $scope.data for typeahead

--- app/js/controllers/edit-ctrl.js
+++ app/js/controllers/edit-ctrl.js
@@ -3,12 +3,32 @@
 angular
 .module('app.controllers')
 .controller('EditCtrl', function($scope, $rootScope, $stateParams, $interval, Restangular, apiDescription, formElementProvider) {
+  $scope.data = {};
+
 	var resourceName = $stateParams.resourceName, 
-		resourceId = $stateParams.id;
+		    resourceId = $stateParams.id;
 
 	var resource = Restangular.one(resourceName, resourceId);
+
 	$scope.rdesc = apiDescription.resource(resourceName);
 	$scope.fep = formElementProvider;
+
+  // Fetch linked resources & 
+  // store them in $scope.data for typeahead
+  
+  _.each($scope.rdesc.fields, function(field){
+
+    var fieldName = field.kind.name,
+        fieldResourceType = field.kind.resourceType; 
+
+    if((fieldName === 'Link') &&
+      !(fieldResourceType in $scope.data)){
+      $scope.data[fieldResourceType] = Restangular.all(fieldResourceType).getList().$object; 
+    } 
+  });
+
+
+
 	$scope.model = resource.get().$object;
 	// $interval(function() { console.log($scope.model); }, 500);
 
