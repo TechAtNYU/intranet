14ff78353d23a47bb1459051c948bc0c592b962c
Clayton Albachten <clay.reed.a@gmail.com>
2015-04-19 16:11:35 -0400

    Improve readability of source code

--- app/js/controllers/edit-ctrl.js
+++ app/js/controllers/edit-ctrl.js
@@ -3,47 +3,59 @@
 angular
 .module('app.controllers')
 .controller('EditCtrl', function($scope, $rootScope, $stateParams, $interval, Restangular, apiDescriptor, formElementProvider) {
+
+	var resourceName = $stateParams.resourceName, 
+	    resourceId   = $stateParams.id;
+	var resource     = Restangular.one(resourceName, resourceId);
+
+	$scope.fep = formElementProvider;
+	$scope.data = {};
+
 	apiDescriptor.then(function(apiDescription) {
 		$scope.rdesc = apiDescription.resource(resourceName);
 		$scope.data = loadLinkedData($scope.rdesc);
 	});
 
-	var resourceName = $stateParams.resourceName;
-	var resourceId = $stateParams.id;
-
-	var resource = Restangular.one(resourceName, resourceId);
-
-	$scope.fep = formElementProvider;
-
-	$scope.data = {};
 	resource.get().then(function(data) {
 		$scope.model = delink(data);
 	});
 
 	$scope.updateResource = function(model, rdesc) {
-		var finalModel = relink(angular.copy(Restangular.stripRestangular(model)), rdesc);
+		var finalModel = 
+			  	relink(
+			  		angular.copy(Restangular.stripRestangular(model)), 
+			  		rdesc
+			  	);
+
 		delete finalModel.modified;
 		delete finalModel.created;
 
 		console.log(finalModel);
-		resource.patch(finalModel).then(function() {
-			alert('Successfully submitted!');
-		}).catch(function(err) {
-			alert('Could not submit to resource. API returned the following error: ' + err);
-		});
+
+		resource.patch(finalModel)
+			.then(function() {
+				alert('Successfully submitted!');
+			}).catch(function(err) {
+				alert(
+					'Could not submit to resource. API returned the following error: '+ 
+					err
+				);
+			});
 	};
 
-	// Fetches linked resources & 
-	// store them in $scope.data for typeahead
+	// Fetch linked resources for the typeahead
 	var loadLinkedData = function(rdesc) {
 		var data = {};
 
 		_.each(rdesc.fields, function(field){
-			var fieldName = field.kind.name,
-				fieldResourceType = field.kind.targetType; 
+			var fieldName 				= field.kind.name,
+	        fieldResourceType = field.kind.targetType; 
 
-			if((fieldName === 'Link') && !(fieldResourceType in data)) {
-				data[fieldResourceType] = Restangular.all(fieldResourceType).getList().$object; 
+			if((fieldName === 'Link') && 
+				!(fieldResourceType in data)) {
+
+				data[fieldResourceType] = 
+			  	Restangular.all(fieldResourceType).getList().$object; 
 			}
 		});
 
@@ -56,44 +68,45 @@ angular
 		_.each(links, function(link, name) {
 			var linkage = link.linkage;
 			
-			// This is primarily to omit the 'self' property
-			if(!linkage) {
-				return;
-			} else if(_.isArray(linkage)) {
-				model[name] = _.pluck(linkage, 'id');
-			} else {
-				model[name] = linkage.id;
+			// Strip API data of "self" property   
+			if(linkage){
+				if(_.isArray(linkage)){
+					model[name] = _.pluck(linkage, 'id');
+				} else {
+					model[name] = linkage.id; 
+				}
 			}
 		});
 
 		return model;
 	};
 
-	// Transforms the linking fields on a model from being located at
-	// model[field.name] to the required structure for linking resources
-	// in the JSON API specification
+	// Transforms linking fields to 
+	// match JSON API specification 
+
 	var relink = function(model, rdesc) {
 		var links = {};
 
 		_.each(rdesc.fields, function(field) {
-			var fieldType = field.kind.name,
-				fieldTargetType = field.kind.targetType,
-				fieldArray = field.kind.isArray; 
+			var fieldType       = field.kind.name,
+				  fieldTargetType = field.kind.targetType,
+				  fieldIsArray 		= field.kind.isArray; 
+
+			var linkage; 
 
 			if(fieldType === 'Link') {
-				var linkage = null;
 
-				if(fieldArray) {
+				if(fieldIsArray) {
 					linkage = _.map(model[field.name], function(value) {
 						return {
 							type: fieldTargetType,
-							id: value
+							  id: value
 						};
 					});
 				} else if(model[field.name]) {
 					linkage = {
 						type: fieldTargetType,
-						id: model[field.name]
+						  id: model[field.name]
 					};
 				} else {
 					linkage = null;
