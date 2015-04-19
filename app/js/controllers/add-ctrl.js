c94104badcc9209c09b3852f2e1c56833898fef7
Clayton Albachten <clay.reed.a@gmail.com>
2015-04-19 16:37:14 -0400

    Write TODOs

--- app/js/controllers/add-ctrl.js
+++ app/js/controllers/add-ctrl.js
@@ -3,76 +3,92 @@
 angular
 .module('app.controllers')
 .controller('AddCtrl', function($scope, $rootScope, $stateParams, $interval, Restangular, apiDescriptor, formElementProvider) {
-	apiDescriptor.then(function(apiDescription) {
-		$scope.rdesc = apiDescription.resource(resourceName);
-		$scope.data = loadLinkedData($scope.rdesc);
-	});
+	// TODO: EditCtrl and AddCtrl should 
+	// share loadLinkedData & relink.  
 
 	var resourceName = $stateParams.resourceName;
+	var resource     = Restangular.all(resourceName);
 
-	var resource = Restangular.all(resourceName);
+	apiDescriptor.then(function(apiDescription) {
+		$scope.rdesc = apiDescription.resource(resourceName);
+		$scope.data  = loadLinkedData($scope.rdesc);
+	});
 
-	$scope.fep = formElementProvider;
-
-	$scope.data = {};
+	$scope.fep   = formElementProvider;
+	$scope.data  = {};
 	$scope.model = {};
 
+	// TODO: change method name 
+	// to createResource 
 	$scope.updateResource = function(model, rdesc) {
-		var finalModel = relink(angular.copy(Restangular.stripRestangular(model)), rdesc);
+		var finalModel = 
+			relink(
+				angular.copy(Restangular.stripRestangular(model)), 
+				rdesc
+			);
+
 		delete finalModel.modified;
 		delete finalModel.created;
+
 		finalModel.type = rdesc.id;
 
 		console.log(finalModel);
-		resource.post(finalModel).then(function() {
-			alert('Successfully submitted!');
-		}).catch(function(err) {
-			alert('Could not submit to resource. API returned the following error: ' + err);
-		});
+
+		resource.post(finalModel)
+			.then(function() {
+				alert('Successfully submitted!');
+			}).catch(function(err) {
+				alert(
+					'Could not submit to resource. API returned the following error: '+ 
+					err
+				);
+			});
 	};
 
-	// Fetches linked resources & 
-	// store them in $scope.data for typeahead
+	// Fetch linked resources for the typeahead
 	var loadLinkedData = function(rdesc) {
 		var data = {};
 
 		_.each(rdesc.fields, function(field){
-			var fieldName = field.kind.name,
-				fieldResourceType = field.kind.targetType; 
+			var fieldName         = field.kind.name,
+				  fieldResourceType = field.kind.targetType; 
 
-			if((fieldName === 'Link') && !(fieldResourceType in data)) {
-				data[fieldResourceType] = Restangular.all(fieldResourceType).getList().$object; 
+			if((fieldName === 'Link') && 
+				!(fieldResourceType in data)) {
+
+					data[fieldResourceType] = 
+						Restangular.all(fieldResourceType).getList().$object; 
 			}
 		});
 
 		return data;
 	};
 
-	// Transforms the linking fields on a model from being located at
-	// model[field.name] to the required structure for linking resources
-	// in the JSON API specification
+	// Transforms linking fields to 
+	// match JSON API specification 
 	var relink = function(model, rdesc) {
 		var links = {};
 
 		_.each(rdesc.fields, function(field) {
-			var fieldType = field.kind.name,
-				fieldTargetType = field.kind.targetType,
-				fieldArray = field.kind.isArray; 
+			var fieldType       = field.kind.name,
+				  fieldTargetType = field.kind.targetType,
+				  fieldIsArray 		= field.kind.isArray; 
+
+			var linkage; 
 
 			if(fieldType === 'Link') {
-				var linkage = null;
 
-				if(fieldArray) {
+				if(fieldIsArray) {
 					linkage = _.map(model[field.name], function(value) {
 						return {
 							type: fieldTargetType,
-							id: value
+							  id: value
 						};
 					});
 				} else if(model[field.name]) {
 					linkage = {
 						type: fieldTargetType,
-						id: model[field.name]
+						  id: model[field.name]
 					};
 				} else {
 					linkage = null;
