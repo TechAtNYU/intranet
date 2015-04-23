6a87cbd58e660477fae55a2ce748151bccd37956
Max Dumas <maltor124@gmail.com>
2015-04-22 21:56:27 -0400

    Add html5mode URL support (link without specified parameter does not work)

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,32 +1,11 @@
 
 angular.module('app')
 .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-	// TODO: Enable this when server is properly configured
-	// $locationProvider.html5Mode(true);
-
-	$urlRouterProvider.otherwise('/');
-
 	$stateProvider
 		.state('index', {
 			url: '/',
 			templateUrl: 'partials/index.html',
-			controller: 'MainCtrl',
-			resolve: {
-				apiDescription: function(Restangular) {
-					return Restangular.all('').getList()
-					.then(function(data) {
-						data = {
-							data: data,
-							resource: function(name) {
-								return _.find(data.data, function(r) {
-									return r.id === name;
-								});
-							}
-						};
-						return data;
-					});
-				}
-			}
+			controller: 'MainCtrl'
 		})
 		.state('list', {
 			url: '/r/:resourceName/list/:selectionMode',
@@ -43,4 +22,8 @@ angular.module('app')
 			templateUrl: 'partials/actions/edit.html',
 			controller: 'EditCtrl'
 		});
+
+	// $urlRouterProvider.otherwise('/');
+
+	$locationProvider.html5Mode(true).hashPrefix('!');
 });
