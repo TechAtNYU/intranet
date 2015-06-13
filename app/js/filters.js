bd23c7bd64b51d8d7f0db5db3ba4f3fe4efe5401
Max Dumas <maltor124@gmail.com>
2015-06-13 12:45:03 -0400

    Fix more JSCS non-compliance

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('app.filters', []).
-  filter('interpolate', function (version) {
-    return function (text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  });
+filter('interpolate', function(version) {
+	return function(text) {
+		return String(text).replace(/\%VERSION\%/mg, version);
+	};
+});
