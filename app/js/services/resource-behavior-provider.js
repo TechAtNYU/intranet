baa377a5e363133a062e7598c69ab9ab194f1931
Max Dumas <maltor124@gmail.com>
2015-06-13 14:32:37 -0400

    Add most functionality to ResourceBehaviorProvider

--- app/js/services/resource-behavior-provider.js
+++ app/js/services/resource-behavior-provider.js
@@ -1,12 +1,42 @@
 angular
 .module('app.services')
-.provider('resourceBehaviorProvider', function() {
+.provider('resourceBehavior', function ResourceBehaviorProvider() {
 	'use strict';
 
 	var defaultBehavior = {};
 	var customBehaviors = [];
+	var actualBehaviors = [];
+
+	var calculateActualBehaviors = function(defaultBehavior, customBehaviors) {
+		if (customBehaviors) {
+			return customBehaviors
+				.map(function(customBehavior) {
+					return angular.merge(customBehavior, defaultBehavior);
+				});
+		}
+
+		return [];
+	};
+
+	this.setDefaultBehavior = function(defaults) {
+		this.defaultBehavior = defaults;
+		actualBehaviors = calculateActualBehaviors(this.defaultBehavior, this.customBehaviors);
+	};
+
+	this.setCustomBehaviors = function(customs) {
+		this.customBehaviors = customs;
+		actualBehaviors = calculateActualBehaviors(this.defaultBehavior, this.customBehaviors);
+	};
 
 	this.$get = function() {
-
+		return {
+			resource: function(resourceName) {
+				if (actualBehaviors[resourceName]) {
+					return actualBehaviors[resourceName];
+				} else {
+					return defaultBehavior;
+				}
+			}
+		};
 	};
 });
