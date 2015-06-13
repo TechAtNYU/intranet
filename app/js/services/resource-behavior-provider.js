aa8ac97b5079092bdecececc28c105ab893511f7
Max Dumas <maltor124@gmail.com>
2015-06-13 13:55:43 -0400

    Add resourceBehaviorProvider

---
+++ app/js/services/resource-behavior-provider.js
@@ -0,0 +1,12 @@
+angular
+.module('app.services')
+.provider('resourceBehaviorProvider', function() {
+	'use strict';
+
+	var defaultBehavior = {};
+	var customBehaviors = [];
+
+	this.$get = function() {
+
+	};
+});
