9356a18f71ee7f265be0619ffcb3f2c14211405b
Ashley Teoh <ashleyteoh@172-16-23-232.DYNAPOOL.NYU.EDU>
2016-10-31 13:31:20 -0400

    Made changes to #147

--- app/js/controllers/actions/organization/organization-list-ctrl.js
+++ app/js/controllers/actions/organization/organization-list-ctrl.js
@@ -27,32 +27,32 @@ angular
 		//mapping organizationID to every liaison in it
 		_.each($scope.data, function(element) {
 					//loop through every liaison in the organization
-					_.each(element.relationships.liaisons.data, function(liaison) {
-						Restangular.one("people/" + liaison.id)
-						.get()
-						.then(function(person) {
-							var details = {
-								'name': person.attributes.name
-							}
+				_.each(element.relationships.liaisons.data, function(liaison) {
+					Restangular.one("people/" + liaison.id)
+					.get()
+					.then(function(person) {
+						var details = {
+							'name': person.attributes.name
+						}
 
-							if (orgIdToLiaisons[element.id] == null) {
-								orgIdToLiaisons[element.id] = [details];
+						if (orgIdToLiaisons[element.id] == null) {
+							orgIdToLiaisons[element.id] = [details];
+						} else {
+							orgIdToLiaisons[element.id].push(details);
+						}
+
+						//generating display text
+						var displayText = "";
+						for (var i = 0; i < orgIdToLiaisons[element.id].length; i++) {
+							if (i == 0) {
+								displayText = orgIdToLiaisons[element.id][i].name;
 							} else {
-								orgIdToLiaisons[element.id].push(details);
+								displayText = displayText + ", " + orgIdToLiaisons[element.id][i].name;
+								$scope.liaisonsDisplay[element.id] = displayText;
 							}
-
-							//generating display text
-							var displayText = "";
-							for (var i = 0; i < orgIdToLiaisons[element.id].length; i++) {
-								if (i == 0) {
-										displayText = orgIdToLiaisons[element.id][i].name;
-								} else {
-									displayText = displayText + ", " + orgIdToLiaisons[element.id][i].name;
-									$scope.liaisonsDisplay[element.id] = displayText;
-								}
-							}
-						});
-					});//end individual liaison loop
+						}
+					});
+				});//end individual liaison loop
 			})
 		});//end Restangular function call
 
