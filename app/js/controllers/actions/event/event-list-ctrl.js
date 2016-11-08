4d3b83866137a1d2209e945cab2d690a114d298f
Ashley Teoh <ashleyteoh@172-16-81-7.DYNAPOOL.NYU.EDU>
2016-11-08 13:22:51 -0500

    updated event listing

--- app/js/controllers/actions/event/event-list-ctrl.js
+++ app/js/controllers/actions/event/event-list-ctrl.js
@@ -2,14 +2,13 @@
 
 angular
 .module('app.controllers')
-.controller('EventListCtrl', function($scope, $rootScope, $stateParams, $state, Restangular, apiDescriptor, dataTransformer) {
+.controller('EventListCtrl', function($scope, $sce, $rootScope, $stateParams, $state, Restangular, apiDescriptor, dataTransformer) {
 	var resourceName = $stateParams.resourceName;
 	var resourceId = $stateParams.id;
 	$scope.resourceName = resourceName;
 	apiDescriptor.then(function(apiDescription) {
 		$scope.rdesc = apiDescription.resource(resourceName);
 	});
-
 
 	$scope.displayDate = function(date) {
 		if (date == undefined) { return; };
@@ -23,7 +22,6 @@ angular
 	var venuesIdToName = {};
 	var organizationIdToName = {};
 	var personIdToName = {};
-	$scope.eventRSVPCount = {};
 
 	$scope.eventDetails = {
 		venue : {},
@@ -36,7 +34,12 @@ angular
 		attendees: {},
 		attendeesCount: {},
 		teaches: {},
-		date: {}
+		date: {},
+		rsvps: {},
+		rsvpCount: {},
+		altRsvps: {},
+		aims: {},
+		categories: {}
 	};
 
 	//mapping teamID to teamName
@@ -73,137 +76,201 @@ angular
 			_.each(person, function(element) {
 				personIdToName[element.id] = element.attributes.name;
 			});
+		})
+		.then(function(){
+			Restangular.all(resourceName)
+			.getList()
+			.then(function(data) {
+				$scope.data = data;
+				if (resourceId) {
+					var index = _.findIndex($scope.data, {id: resourceId});
+					$scope.model = $scope.data[index];
+				}
+
+				_.each($scope.data, function(element) {
+					//mapping eventID to venue names with links
+					if (element.relationships.venue.data != null) {
+						// !! change venueURL to the venue page once the venue override page is ready
+						var venueURL = "https://api.tnyu.org/v3/venues/" + element.relationships.venue.data.id;
+						$scope.eventDetails.venue[element.id] = "<a href=" + venueURL + ">" + venuesIdToName[element.relationships.venue.data.id] + "</a>";
+					};//end
+
+					//mapping eventID to categories
+					if (element.attributes.categories.length > 0) {
+						element.attributes.categories.forEach(category => {
+							if ($scope.eventDetails.categories[element.id] == undefined) {
+								$scope.eventDetails.categories[element.id] = category;
+							} else {
+								var string = $scope.eventDetails.categories[element.id];
+								string = string + ", " + category;
+								$scope.eventDetails.categories[element.id] = string;
+							}
+						})
+					};//end
+
+					//mapping eventID to aims
+					if (element.attributes.aims.length > 0) {
+						element.attributes.aims.forEach(aim => {
+							if ($scope.eventDetails.aims[element.id] == undefined) {
+								$scope.eventDetails.aims[element.id] = aim;
+							} else {
+								var string = $scope.eventDetails.aims[element.id];
+								string = string + ", " + aim;
+								$scope.eventDetails.aims[element.id] = string;
+							}
+						})
+					};//end
+
+					//mapping eventID to team names
+					if (element.relationships.teams.data.length > 0) {
+						element.relationships.teams.data.forEach( team => {
+							if ($scope.eventDetails.team[element.id] == undefined) {
+								$scope.eventDetails.team[element.id] = teamsIdToName[team.id];
+							} else {
+								var string = $scope.eventDetails.team[element.id];
+								string = string + ", " + teamsIdToName[team.id];
+								$scope.eventDetails.team[element.id] = string;
+							}
+						});
+					};//end
+
+					//mapping eventID to survey URI
+					if (element.relationships.survey.data != null) {
+						Restangular.one("surveys/" + element.relationships.survey.data.id)
+						.get()
+						.then(function(survey) {
+							$scope.eventDetails.survey[element.id] = survey.attributes.URI;
+						});
+					};//end
+
+					//mapping eventID to co-organizers
+					if (element.relationships.coorganizers.data != null) {
+						element.relationships.coorganizers.data.forEach( organization => {
+							if ($scope.eventDetails.coorganizers[element.id] == undefined) {
+								$scope.eventDetails.coorganizers[element.id] = organizationIdToName[organization.id];
+							} else {
+								var string = $scope.eventDetails.coorganizers[element.id];
+								string = string + ", " + organizationIdToName[organization.id];
+								$scope.eventDetails.coorganizers[element.id] = string;
+							}
+						})
+					};//end
+
+					//mapping eventID to attendees & number of attendees
+					$scope.eventDetails.attendeesCount[element.id] = element.relationships.attendees.data.length;
+					if (element.relationships.attendees.data.length > 0) {
+						element.relationships.attendees.data.forEach(data => {
+							if ($scope.eventDetails.attendees[element.id] === undefined) {
+								$scope.eventDetails.attendees[element.id] = personIdToName[data.id];
+							} else {
+								var string = $scope.eventDetails.attendees[element.id];
+								string = string + ", " + personIdToName[data.id];
+								$scope.eventDetails.attendees[element.id] = string;
+							};
+						});
+					}//end
+
+					//mapping eventID to feedback
+					if (element.relationships.feedback.data.length > 0) {
+						element.relationships.feedback.data.forEach(response => {
+							Restangular.one("survey-responses/" + response.id)
+							.get()
+							.then(function(data) {
+								if ($scope.eventDetails.feedback[element.id] == undefined) {
+									$scope.eventDetails.feedback[element.id] = data.id;
+								} else {
+									var string = $scope.eventDetails.feedback[element.id];
+									string = string + ", " + data.id;
+									$scope.eventDetails.feedback[element.id] = string;
+								}
+							});
+						})
+					}//end
+
+					//mapping eventID to list and number of RSVPs
+					$scope.eventDetails.rsvpCount[element.id] = element.relationships.rsvps.data.length;
+					if (element.relationships.rsvps.data.length > 0) {
+						element.relationships.rsvps.data.forEach(data => {
+							if ($scope.eventDetails.rsvps[element.id] == undefined) {
+								$scope.eventDetails.rsvps[element.id] = personIdToName[data.id];
+							} else {
+								var string = $scope.eventDetails.rsvps[element.id];
+								string = string + ", " + personIdToName[data.id];
+								$scope.eventDetails.rsvps[element.id] = string;
+							}
+						})
+					}//end
+
+					//mapping eventID to alt RSVP urls
+					if (element.attributes.alternateRsvpUrls.length > 0) {
+						element.attributes.alternateRsvpUrls.forEach(url => {
+							if ($scope.eventDetails.altRsvps[element.id] == undefined) {
+								$scope.eventDetails.altRsvps[element.id] = "<a href=" + url + "</a>" + url;
+							} else {
+								var string = $scope.eventDetails.altRsvps[element.id];
+								string = string + ", " + "<a href=" + url + "</a>" + url;
+								$scope.eventDetails.altRsvps[element.id] = string;
+							}
+						})
+					}//end
+
+					//mapping eventID to teaches
+					if (element.relationships.teaches.data.length > 0) {
+						element.relationships.teaches.data.forEach(response => {
+							Restangular.one("skills/" + response.id)
+							.get()
+							.then(function(data) {
+								if ($scope.eventDetails.teaches[element.id] == undefined) {
+									$scope.eventDetails.teaches[element.id] = data.attributes.name;
+								} else {
+									var string = $scope.eventDetails.teaches[element.id];
+									string = string + ", " + data.attributes.name;
+									$scope.eventDetails.teaches[element.id] = string;
+								}
+							});
+						})
+					}//end
+
+					//mapping eventID to addedBy
+					if (element.relationships.addedBy != null && element.relationships.addedBy.data != null ) {
+						Restangular.one("events/" + element.id + "?include=addedBy")
+						.get()
+						.then(function(data) {
+							if (data.length > 0) {
+								for (var i = 0; i < data.length; i ++) {
+									if ($scope.eventDetails.addedBy[element.id] == undefined) {
+										$scope.eventDetails.addedBy[element.id] = data[i].attributes.name;
+									} else {
+										var string = $scope.eventDetails.addedBy[element.id];
+										string = string + ", " + data[i].attributes.name;
+										$scope.eventDetails.addedBy[element.id] = string;
+									}
+								}
+							}
+						})
+					}
+
+					//mapping eventID to presenters
+					if (element.relationships.presenters.data.length > 0 ) {
+						Restangular.one("events/" + element.id + "?include=presenters")
+						.get()
+						.then(function(data) {
+							if (data.length > 0) {
+								for (var i = 0; i < data.length; i ++) {
+									if ($scope.eventDetails.presenters[element.id] == undefined) {
+										$scope.eventDetails.presenters[element.id] = data[i].attributes.name;
+									} else {
+										var string = $scope.eventDetails.presenters[element.id];
+										string = string + ", " + data[i].attributes.name;
+										$scope.eventDetails.presenters[element.id] = string;
+									}
+								}
+							}
+						})
+					}
+				});//end _.each()
+			});
 		});
-
-	Restangular.all(resourceName)
-	.getList()
-	.then(function(data) {
-		$scope.data = data;
-		console.log($scope.data[0]);
-		if (resourceId) {
-			var index = _.findIndex($scope.data, {id: resourceId});
-			$scope.model = $scope.data[index];
-		}
-
-		_.each($scope.data, function(element) {
-			//mapping eventID to venue names
-			if (element.relationships.venue.data != null) {
-				$scope.eventDetails.venue[element.id] = venuesIdToName[element.relationships.venue.data.id];
-			};//end
-
-			//mapping eventID to team names
-			if (element.relationships.teams.data.length > 0) {
-				element.relationships.teams.data.forEach( team => {
-					if ($scope.eventDetails.team[element.id] == undefined) {
-						$scope.eventDetails.team[element.id] = teamsIdToName[team.id];
-					} else {
-						var string = $scope.eventDetails.team[element.id];
-						string = string + ", " + teamsIdToName[team.id];
-						$scope.eventDetails.team[element.id] = string;
-					}
-				});
-			};//end
-
-			//mapping eventID to survey URI
-			if (element.relationships.survey.data != null) {
-				Restangular.one("surveys/" + element.relationships.survey.data.id)
-				.get()
-				.then(function(survey) {
-					$scope.eventDetails.survey[element.id] = survey.attributes.URI;
-				});
-			};//end
-
-			//mapping eventID to co-organizers
-			if (element.relationships.coorganizers.data != null) {
-				element.relationships.coorganizers.data.forEach( organization => {
-					if ($scope.eventDetails.coorganizers[element.id] == undefined) {
-						$scope.eventDetails.coorganizers[element.id] = organizationIdToName[organization.id];
-					} else {
-						var string = $scope.eventDetails.coorganizers[element.id];
-						string = string + ", " + organizationIdToName[organization.id];
-						$scope.eventDetails.coorganizers[element.id] = string;
-					}
-				})
-			};//end
-
-			//mapping eventID to attendees & number of attendees
-			$scope.eventDetails.attendeesCount[element.id] = element.relationships.attendees.data.length;
-			if (element.relationships.attendees.data.length > 0) {
-				element.relationships.attendees.data.forEach(response => {
-					if ($scope.eventDetails.attendees[element.id] === undefined) {
-						$scope.eventDetails.attendees[element.id] = personIdToName[response.id];
-					} else {
-						var string = $scope.eventDetails.attendees[element.id];
-						string = string + ", " + personIdToName[response.id];
-						$scope.eventDetails.attendees[element.id] = string;
-					}
-				});
-			}//end
-
-			//mapping eventID to number of RSVPs
-			$scope.eventRSVPCount[element.id] = element.relationships.rsvps.data.length;
-
-			//mapping eventID to feedback
-			if (element.relationships.feedback.data.length > 0) {
-				element.relationships.feedback.data.forEach(response => {
-					Restangular.one("survey-responses/" + response.id)
-					.get()
-					.then(function(data) {
-						if ($scope.eventDetails.feedback[element.id] == undefined) {
-							$scope.eventDetails.feedback[element.id] = data.id;
-						} else {
-							var string = $scope.eventDetails.feedback[element.id];
-							string = string + ", " + data.id;
-							$scope.eventDetails.feedback[element.id] = string;
-						}
-					});
-				})
-			}//end
-
-			//mapping eventID to teaches
-			if (element.relationships.teaches.data.length > 0) {
-				element.relationships.teaches.data.forEach(response => {
-					Restangular.one("skills/" + response.id)
-					.get()
-					.then(function(data) {
-						if ($scope.eventDetails.teaches[element.id] == undefined) {
-							$scope.eventDetails.teaches[element.id] = data.attributes.name;
-						} else {
-							var string = $scope.eventDetails.teaches[element.id];
-							string = string + ", " + data.attributes.name;
-							$scope.eventDetails.teaches[element.id] = string;
-						}
-					});
-				})
-			}//end
-
-			//mapping eventID to addedBy
-			if (element.relationships.addedBy != null && element.relationships.addedBy.data != null ) {
-				Restangular.one("people/" + element.relationships.addedBy.data.id)
-				.get()
-				.then(function(data) {
-						$scope.eventDetails.addedBy[element.id] = data.attributes.name;
-				});
-			}
-
-			//mapping eventID to presenters
-			if (element.relationships.presenters.data.length > 0 ) {
-				element.relationships.presenters.data.forEach(presenter => {
-					Restangular.one("presenters/" + presenter.id)
-					.get()
-					.then(function(data) {
-						if ($scope.eventDetails.presenters[element.id] == undefined) {
-							$scope.eventDetails.presenters[element.id] = data.attributes.name;
-						} else {
-							var string = $scope.eventDetails.presenters[element.id];
-							string = string + ", " + data.attributes.name;
-							$scope.eventDetails.presenters[element.id] = string;
-						}
-					});
-				})
-			}
-		});//end _.each()
-	});
 
 	$scope.updateSelection = function(newModelId) {
 		var index =	_.findIndex($scope.data, {'id': newModelId});
