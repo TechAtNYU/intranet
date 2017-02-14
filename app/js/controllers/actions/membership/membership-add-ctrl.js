e8a62ba46f7823c84b2c115900a6f678963c66dc
Ashley Teoh <atq203@nyu.edu>
2017-02-14 15:39:37 -0500

    fixed line spacing

--- app/js/controllers/actions/membership/membership-add-ctrl.js
+++ app/js/controllers/actions/membership/membership-add-ctrl.js
@@ -5,6 +5,7 @@ angular
 .controller('MembershipAddCtrl', function($scope, $rootScope, $stateParams, $state,
 		$interval, Restangular, apiDescriptor, formElementProvider, dataTransformer) {
 
+
 	var resourceName = $stateParams.resourceName;
 
 	var resource = Restangular.all(resourceName);
