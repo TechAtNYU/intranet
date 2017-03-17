a7f5a1b6f2800f9c678eac5665aa58423cddc207
Ashley Teoh <atq203@nyu.edu>
2017-03-17 16:07:18 -0400

    position is displayed as it's name, not id

--- app/js/controllers/actions/membership/membership-add-ctrl.js
+++ app/js/controllers/actions/membership/membership-add-ctrl.js
@@ -15,6 +15,30 @@ angular
 	$scope.data = {};
 	$scope.model = {attributes: {}};
 
+	let teamsIdToName = {};
+	$scope.positionIdToName = {};
+
+	//mapping teamID to teamName
+	Restangular.one('teams')
+	.getList()
+	.then(function(teams) {
+		_.each(teams, function(element) {
+			teamsIdToName[element.id] = element.attributes.name;
+		});
+
+		//mapping positionID to names
+		Restangular.one('positions')
+		.getList()
+		.then( position => {
+			_.each(position, function(element) {
+				const isLead = element.attributes.isLead;
+				const teamName = teamsIdToName[element.relationships.team.data.id];
+				$scope.positionIdToName[element.id] = `${teamName} ${(isLead ? '- Lead' : '')}`;
+			});
+		});
+	});
+
+
 	apiDescriptor.then(function(apiDescription) {
 		$scope.rdesc = apiDescription.resource(resourceName);
 		$scope.data = dataTransformer.loadLinkedData($scope.rdesc, $scope.refreshData);
--- app/js/controllers/actions/membership/membership-edit-ctrl.js
+++ app/js/controllers/actions/membership/membership-edit-ctrl.js
@@ -21,6 +21,28 @@ angular
 		$scope.model = dataTransformer.delink(data);
 	});
 
+	let teamsIdToName = {};
+	$scope.positionIdToName = {};
+	//mapping teamID to teamName
+	Restangular.one('teams')
+	.getList()
+	.then(function(teams) {
+		_.each(teams, function(element) {
+			teamsIdToName[element.id] = element.attributes.name;
+		});
+
+		//mapping positionID to names
+		Restangular.one('positions')
+		.getList()
+		.then( position => {
+			_.each(position, function(element) {
+				const isLead = element.attributes.isLead;
+				const teamName = teamsIdToName[element.relationships.team.data.id];
+				$scope.positionIdToName[element.id] = `${teamName} ${(isLead ? '- Lead' : '')}`;
+			});
+		});
+	});
+
 	$scope.updateResource = function (model, rdesc) {
 		dataTransformer.updateResource(model, rdesc, resource).then(function (data) {
 			$state.go('list', {resourceName: resourceName, selectionMode: 'single', id: data.id})
