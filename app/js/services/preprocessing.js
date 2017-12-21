e7c7d7b3ee9be01dc9418b64eb40e801f17dab6d
ashley <msashleychen@gmail.com>
2017-12-21 17:15:33 -0500

    Added venueIdToNames to preProcess

--- app/js/services/preprocessing.js
+++ app/js/services/preprocessing.js
@@ -14,6 +14,17 @@ angular
                 });
             return teamsIdToName;
         },
+        venueIdToNames: function() {
+            var venuesIdToName = {};
+            Restangular.all('venues')
+                .getList()
+                .then(function(teams) {
+                    _.each(teams, function(element) {
+                        venuesIdToName[element.id] = element.attributes.name;
+                    });
+                });
+            return venuesIdToName;
+        },
         positionToString: function(teamMap, element, includeLead){
             if(includeLead){
                 return formatTeamDisplayFilter(teamMap[element.relationships.team.data.id], element.attributes.isLead);
