1ad1c2b21358e4964acc8baaa1dbd82e48fd3cb6
Ashley Teoh <atq203@nyu.edu>
2018-02-01 20:32:53 -0500

    spacing

--- app/js/controllers/main-ctrl.js
+++ app/js/controllers/main-ctrl.js
@@ -5,15 +5,15 @@ angular
 .controller('MainCtrl', function($scope, apiDescriptor) {
 	apiDescriptor.then(function(apiDescription) {
 
-    $scope.links = {
-      '✨ Public site': 'http://techatnyu.org',
-      'Public site (Staging)': 'http://intranet-staging.tnyu.org/#/',
-      'Intranet (Staging)': 'http://intranet-staging.tnyu.org/#/',
-      '📆 Calendar': 'http://cal.techatnyu.org',
-      'Calendar (Staging)': 'http://cal.techatnyu.org',
-      '🙋🏻 RSVP': 'http://rsvp.techatnyu.org',
-      'RSVP (Staging)': 'http://rsvp-staging.techatnyu.org'
-    }
+		$scope.links = {
+			'✨ Public site': 'http://techatnyu.org',
+			'Public site (Staging)': 'http://intranet-staging.tnyu.org/#/',
+			'Intranet (Staging)': 'http://intranet-staging.tnyu.org/#/',
+			'📆 Calendar': 'http://cal.techatnyu.org',
+			'Calendar (Staging)': 'http://cal.techatnyu.org',
+			'🙋🏻 RSVP': 'http://rsvp.techatnyu.org',
+			'RSVP (Staging)': 'http://rsvp-staging.techatnyu.org'
+		}
 
 		$scope.apidesc = apiDescription.data;
 	});
