3052085c86dac7aee3c8c63c9ae9a97767808d6e
ashley teoh <ashleytqy@gmail.com>
2018-02-01 23:04:39 -0500

    use prettier (#193)
    * use prettier
    
    * missing quotes
    
    * add script

--- app/js/controllers/actions/membership/membership-edit-ctrl.js
+++ app/js/controllers/actions/membership/membership-edit-ctrl.js
@@ -1,56 +1,83 @@
-'use strict';
+"use strict";
 
 angular
-.module('app.controllers')
-.controller('MembershipEditCtrl', function($scope, $rootScope, $stateParams, $state,
-		$interval, formatTeamDisplayFilter, Restangular, apiDescriptor, formElementProvider, dataTransformer) {
+  .module("app.controllers")
+  .controller("MembershipEditCtrl", function(
+    $scope,
+    $rootScope,
+    $stateParams,
+    $state,
+    $interval,
+    formatTeamDisplayFilter,
+    Restangular,
+    apiDescriptor,
+    formElementProvider,
+    dataTransformer
+  ) {
+    const resourceName = $stateParams.resourceName;
+    const resourceId = $stateParams.id;
 
-	var resourceName = $stateParams.resourceName;
-	var resourceId = $stateParams.id;
+    const resource = Restangular.one(resourceName, resourceId);
 
-	var resource = Restangular.one(resourceName, resourceId);
+    $scope.fep = formElementProvider;
 
-	$scope.fep = formElementProvider;
+    $scope.data = {};
+    resource.get().then(function(data) {
+      apiDescriptor.then(function(apiDescription) {
+        $scope.rdesc = apiDescription.resource(resourceName);
+        $scope.data = dataTransformer.loadLinkedData(
+          $scope.rdesc,
+          $scope.refreshData
+        );
+      });
+      $scope.model = dataTransformer.delink(data);
+    });
 
-	$scope.data = {};
-	resource.get().then(function(data) {
-		apiDescriptor.then(function(apiDescription) {
-			$scope.rdesc = apiDescription.resource(resourceName);
-			$scope.data = dataTransformer.loadLinkedData($scope.rdesc, $scope.refreshData);
-		});
-		$scope.model = dataTransformer.delink(data);
-	});
+    const teamsIdToName = {};
+    $scope.positionIdToName = {};
+    //mapping teamID to teamName
+    Restangular.one("teams")
+      .getList()
+      .then(function(teams) {
+        _.each(teams, function(element) {
+          teamsIdToName[element.id] = element.attributes.name;
+        });
 
-	let teamsIdToName = {};
-	$scope.positionIdToName = {};
-	//mapping teamID to teamName
-	Restangular.one('teams')
-	.getList()
-	.then(function(teams) {
-		_.each(teams, function(element) {
-			teamsIdToName[element.id] = element.attributes.name;
-		});
+        //mapping positionID to names
+        Restangular.one("positions")
+          .getList()
+          .then(position => {
+            _.each(position, function(element) {
+              const isLead = element.attributes.isLead;
+              const teamName =
+                teamsIdToName[element.relationships.team.data.id];
+              $scope.positionIdToName[element.id] = formatTeamDisplayFilter(
+                teamName,
+                isLead
+              );
+              $scope.refreshData(
+                $scope.data,
+                $scope.rdesc.attributes.fields[1].kind["target-type"]
+              );
+            });
+          });
+      });
 
-		//mapping positionID to names
-		Restangular.one('positions')
-		.getList()
-		.then( position => {
-			_.each(position, function(element) {
-				const isLead = element.attributes.isLead;
-				const teamName = teamsIdToName[element.relationships.team.data.id];
-				$scope.positionIdToName[element.id] = formatTeamDisplayFilter(teamName, isLead);
-				$scope.refreshData($scope.data, $scope.rdesc.attributes.fields[1].kind['target-type']);
-			});
-		});
-	});
+    $scope.updateResource = function(model, rdesc) {
+      dataTransformer
+        .updateResource(model, rdesc, resource)
+        .then(function(data) {
+          $state.go("list", {
+            resourceName: resourceName,
+            selectionMode: "single",
+            id: data.id
+          });
+        });
+    };
 
-	$scope.updateResource = function (model, rdesc) {
-		dataTransformer.updateResource(model, rdesc, resource).then(function (data) {
-			$state.go('list', {resourceName: resourceName, selectionMode: 'single', id: data.id})
-		});
-	}
-
-	$scope.refreshData = function(data, fieldResourceType) {
-		data[fieldResourceType] = Restangular.all(fieldResourceType).getList().$object;
-	};
-});
+    $scope.refreshData = function(data, fieldResourceType) {
+      data[fieldResourceType] = Restangular.all(
+        fieldResourceType
+      ).getList().$object;
+    };
+  });
