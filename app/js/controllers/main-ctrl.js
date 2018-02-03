1c4df6ba8f3fe1d63715933c13cec6ab7b1516ce
Ashley Teoh <atq203@nyu.edu>
2018-02-02 19:48:39 -0500

    remove links

--- app/js/controllers/main-ctrl.js
+++ app/js/controllers/main-ctrl.js
@@ -4,17 +4,6 @@ angular
 .module('app.controllers')
 .controller('MainCtrl', function($scope, apiDescriptor) {
 	apiDescriptor.then(function(apiDescription) {
-
-		$scope.links = {
-			'✨ Public site': 'http://techatnyu.org',
-			'Public site (Staging)': 'http://intranet-staging.tnyu.org/#/',
-			'Intranet (Staging)': 'http://intranet-staging.tnyu.org/#/',
-			'📆 Calendar': 'http://cal.techatnyu.org',
-			'Calendar (Staging)': 'http://cal.techatnyu.org',
-			'🙋🏻 RSVP': 'http://rsvp.techatnyu.org',
-			'RSVP (Staging)': 'http://rsvp-staging.techatnyu.org'
-		}
-
 		$scope.apidesc = apiDescription.data;
 	});
 });
