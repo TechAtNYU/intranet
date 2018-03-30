a3dca58d04126a4df11ab9828deb5c6effb4617e
ashley <msashleychen@gmail.com>
2018-03-29 20:03:26 -0400

    Refactored incomes list controller

--- app/js/controllers/actions/incomes/incomes-list-ctrl.js
+++ app/js/controllers/actions/incomes/incomes-list-ctrl.js
@@ -22,8 +22,7 @@ angular
 		}
 
 		_.each($scope.data, element => {
-			element = preProcess.changeDate(element);
-			element.attributes.recievedOn = preProcess.convertTimeToEST(element.attributes.recievedOn);
+			element = preProcess.changeDate(element, Array("recievedOn"));
 			Restangular.one("people/" + element.relationships.authorizer.data.id)
 			.get()
 			.then(data => {
