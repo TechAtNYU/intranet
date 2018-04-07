17b33ebd86e7c8be81cf6bef4613c9479cb00ecc
ashley <msashleychen@gmail.com>
2018-04-07 16:22:50 -0400

    Debugging edit bug

--- app/js/controllers/actions/event/event-edit-ctrl.js
+++ app/js/controllers/actions/event/event-edit-ctrl.js
@@ -7,31 +7,21 @@ angular
 
 	var resourceName = $stateParams.resourceName;
 	var resourceId = $stateParams.id;
+
 	var resource = Restangular.one(resourceName, resourceId);
 
 	$scope.fep = formElementProvider;
 
 	$scope.data = {};
-	$scope.statusFound = false;
-	
-
 	resource.get().then(function(data) {
 		apiDescriptor.then(function(apiDescription) {
 			$scope.rdesc = apiDescription.resource(resourceName);
 			$scope.data = dataTransformer.loadLinkedData($scope.rdesc, $scope.refreshData);
-			
-			if($scope.rdesc.attributes.fields[18].name == "status"){
-				$scope.statusFound = true;
-			}
 		});
 		$scope.model = dataTransformer.delink(data);
-		$scope.statusData = $scope.model.attributes.status;
 	});
 
-	$scope.updateResource = function (model, rdesc, statusData) {
-		if(statusFound !=0){
-			model.attributes.status = statusData;
-		}
+	$scope.updateResource = function (model, rdesc) {
 		dataTransformer.updateResource(model, rdesc, resource).then(function (data) {
 			$state.go('list', {resourceName: resourceName, selectionMode: 'single', id: data.id})
 		});
