0f3367619ffbfbc57a75a9cf98ba23c3566d8cfd
ashley <msashleychen@gmail.com>
2018-04-09 17:16:22 -0400

    Fixed event edit controller

--- app/js/controllers/actions/event/event-edit-ctrl.js
+++ app/js/controllers/actions/event/event-edit-ctrl.js
@@ -11,17 +11,23 @@ angular
 	var resource = Restangular.one(resourceName, resourceId);
 
 	$scope.fep = formElementProvider;
+	$scope.statusFound = false;
 
 	$scope.data = {};
 	resource.get().then(function(data) {
 		apiDescriptor.then(function(apiDescription) {
 			$scope.rdesc = apiDescription.resource(resourceName);
 			$scope.data = dataTransformer.loadLinkedData($scope.rdesc, $scope.refreshData);
+			$scope.statusFound = true;
 		});
 		$scope.model = dataTransformer.delink(data);
+		$scope.statusData = $scope.model.attributes.status;
 	});
 
-	$scope.updateResource = function (model, rdesc) {
+	$scope.updateResource = function (model, rdesc, updatedStatus) {
+		if(updatedStatus != undefined){
+			model.attributes.status = updatedStatus;
+		}
 		dataTransformer.updateResource(model, rdesc, resource).then(function (data) {
 			$state.go('list', {resourceName: resourceName, selectionMode: 'single', id: data.id})
 		});
